0551fbdd7bab84b53f3568a5367a5db23794e14c
sid-bhatt <70953578+sid-bhatt@users.noreply.github.com>
2021-05-25 16:48:15 +0530

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -154,83 +154,49 @@ const states = [
   }
 ];
 
-function Townprint(props) {
-  return (
-    <>
-      <li id={"town" + (props.index + 1)}>{props.towns.name}</li>
-    </>
-  );
-}
-
-function Town(props) {
-  return (
-    <>
-      <ul>
-        {props.towns.map((town, index) => {
-          return <Townprint index={index} towns={town}></Townprint>;
-        })}
-      </ul>
-    </>
-  );
-}
-
-function Cityprint(props) {
-  const [getFlag, setFlag] = useState(false);
-
-  const hideANDshow = (e) => {
-    e.stopPropagation();
-    if (getFlag) setFlag(false);
-    else setFlag(true);
-  };
-  return (
-    <>
-      <li id={"city" + (props.index + 1)} onClick={hideANDshow}>
-        {props.cities.name}
-        {getFlag && <Town towns={props.cities.towns}></Town>}
-      </li>
-    </>
-  );
-}
-
-function City(props) {
-  return (
-    <>
-      <ul>
-        {props.cities.map((city, index) => {
-          return <Cityprint index={index} cities={city}></Cityprint>;
-        })}
-      </ul>
-    </>
-  );
-}
-
-function State(props) {
-  const [getFlag, setFlag] = useState(false);
-
-  const hideANDshow2 = (e) => {
-    e.stopPropagation();
-    if (getFlag) setFlag(false);
-    else setFlag(true);
-  };
-
-  return (
-    <>
-      <li id={"state" + (props.index + 1)} onClick={hideANDshow2}>
-        {props.states.name}
-        {getFlag && <City cities={props.states.cities}></City>}
-      </li>
-    </>
-  );
-}
-
 function App() {
+  const [showCities, setShowCities] = useState(false);
+  const [showTowns, setShowTowns] = useState(false);
+
+  const toggleState = () => {
+    setShowCities(!showCities);
+  };
+
+  const toggleCities = () => {
+    setShowTowns(!showTowns);
+  };
+
   return (
     <div id="main">
-      <ul>
-        {states.map((state, index) => {
-          return <State index={index}>{state}</State>;
-        })}
-      </ul>
+      {states.map((stateName, index) => {
+        return (
+          <div key={index}>
+            <h2 id={`state${index + 1}`} onclick={toggleState}>
+              {stateName.name}
+            </h2>
+            {stateName.citites.map((subItem1, i) => {
+              return (
+                <div key={i}>
+                  {showCities ? (
+                    <h3 id={`city${i + 1}`} onclick={toggleCities}>
+                      {subItem1.name}
+                    </h3>
+                  ) : null}
+                  {subItem1.towns.map((subItem2, j) => {
+                    return (
+                      <div>
+                        {showTowns ? (
+                          <h5 id={`town${j + 1}`}>{subItem2.name}</h5>
+                        ) : null}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
