87f35f0d9dfc2ab5cc0242f265eaeff12f9148d3
sid-bhatt <70953578+sid-bhatt@users.noreply.github.com>
2021-05-25 16:20:27 +0530

    k

--- src/components/App.js
+++ src/components/App.js
@@ -10,33 +10,33 @@ const states = [
         name: "Indore",
         towns: [
           {
-            name: "Mhow",
+            name: "Mhow"
           },
           {
-            name: "Dewas",
-          },
-        ],
+            name: "Dewas"
+          }
+        ]
       },
       {
         name: "Bhopal",
         towns: [
           {
-            name: "Manit",
+            name: "Manit"
           },
           {
-            name: "Berasia",
-          },
-        ],
+            name: "Berasia"
+          }
+        ]
       },
       {
         name: "Gwalior",
         towns: [
           {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
+            name: "Ajaypur"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Jharkhand",
@@ -45,36 +45,36 @@ const states = [
         name: "Dhanbad",
         towns: [
           {
-            name: "IIT(ISM) Dhanbad",
+            name: "IIT(ISM) Dhanbad"
           },
           {
-            name: "Hirapur",
-          },
-        ],
+            name: "Hirapur"
+          }
+        ]
       },
       {
         name: "Wasseypur",
         towns: [
           {
-            name: "Sardar khan's",
+            name: "Sardar khan's"
           },
           {
-            name: "Faizal khan's",
-          },
-        ],
+            name: "Faizal khan's"
+          }
+        ]
       },
       {
         name: "Mirzapur",
         towns: [
           {
-            name: "Kaleen bhaiya's",
+            name: "Kaleen bhaiya's"
           },
           {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
+            name: "Guddu bhaiya's"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Assam",
@@ -83,36 +83,36 @@ const states = [
         name: "Guwhati",
         towns: [
           {
-            name: "Amin",
+            name: "Amin"
           },
           {
-            name: "Jalah",
-          },
-        ],
+            name: "Jalah"
+          }
+        ]
       },
       {
         name: "Jungle1",
         towns: [
           {
-            name: "Tiger found at IIT Guwahati",
+            name: "Tiger found at IIT Guwahati"
           },
           {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
+            name: "Leopard found in IIT Guwahati"
+          }
+        ]
       },
       {
         name: "Tezpur",
         towns: [
           {
-            name: "Dibrugarh",
+            name: "Dibrugarh"
           },
           {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
+            name: "Silchar"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Bihar",
@@ -121,41 +121,118 @@ const states = [
         name: "Patna",
         towns: [
           {
-            name: "Sonpur",
+            name: "Sonpur"
           },
           {
-            name: "Maner",
-          },
-        ],
+            name: "Maner"
+          }
+        ]
       },
       {
         name: "Gaya",
         towns: [
           {
-            name: "Bakraur",
+            name: "Bakraur"
           },
           {
-            name: "Barachatti",
-          },
-        ],
+            name: "Barachatti"
+          }
+        ]
       },
       {
         name: "Darbhanga",
         towns: [
           {
-            name: "Singhwara",
+            name: "Singhwara"
           },
           {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
+            name: "Jale"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
+function Townprint(props) {
+  return (
+    <>
+      <li id={"town" + (props.index + 1)}>{props.towns.name}</li>
+    </>
+  );
+}
+
+function Town(props) {
+  return (
+    <>
+      <ul>
+        {props.towns.map((town, index) => {
+          return <Townprint index={index} towns={town}></Townprint>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+function Cityprint(props) {
+  const [getFlag, setFlag] = useState(false);
+
+  const hideANDshow = (e) => {
+    e.stopPropagation();
+    if (getFlag) setFlag(false);
+    else setFlag(true);
+  };
+  return (
+    <>
+      <li id={"city" + (props.index + 1)} onClick={hideANDshow}>
+        {props.cities.name}
+        {getFlag && <Town towns={props.cities.towns}></Town>}
+      </li>
+    </>
+  );
+}
+
+function City(props) {
+  return (
+    <>
+      <ul>
+        {props.cities.map((city, index) => {
+          return <Cityprint index={index} cities={city}></Cityprint>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+function State(props) {
+  const [getFlag, setFlag] = useState(false);
+
+  const hideANDshow2 = (e) => {
+    e.stopPropagation();
+    if (getFlag) setFlag(false);
+    else setFlag(true);
+  };
+
+  return (
+    <>
+      <li id={"state" + (props.index + 1)} onClick={hideANDshow2}>
+        {props.states.name}
+        {getFlag && <City cities={props.states.cities}></City>}
+      </li>
+    </>
+  );
+}
+
 function App() {
-  return <div id="main"></div>;
+  return (
+    <div id="main">
+      <ul>
+        {states.map((state, index) => {
+          return <State index={index}>{state}</State>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
